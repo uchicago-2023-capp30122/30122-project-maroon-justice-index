1ae5048f2af0d0d46628da96f82c557b741f8bf6
Betty Fang <bfang@uchicago.edu>
2023-02-14 01:07:57 -0600

    removed \n\t

--- cleaning/idhs_cleaning.py
+++ cleaning/idhs_cleaning.py
@@ -10,7 +10,8 @@ with open("../webscraping/idhs.json", "r") as fj:
     for office in data:
         office["Address"] = office["Address"].strip()
         office["Contact"] = office["Contact"].strip()
-        #office["Address"] = office["Address"].replace("\n",'')
+        office["Contact"] = re.sub(r"[\t]", "", office["Contact"])
+        office["Contact"] = re.sub(r"[\n+]", " ", office["Contact"])
 
 
 with open("cleaned_idhs.json", "w") as fj2:
