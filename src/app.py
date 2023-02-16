92c64e01b25f296de09b24d6067d43973b037634
Ivanna Melissa Rodriguez <ivannar@linux3.cs.uchicago.edu>
2023-02-15 20:13:47 -0600

    exploring dash

--- src/app.py
+++ src/app.py
@@ -4,31 +4,60 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
-app = Dash(__name__)
+external_stylesheets = [
+    "https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"
+]
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+# ------------------ data and map here --------------------------------
+
+neighborhoods = "https://raw.githubusercontent.com/blackmad/neighborhoods/master/chicago.geojson"
+
+# generate some data for each region defined in geojson...
+df = pd.DataFrame(
+    {"fips": range(1, 77, 1), "unemp": np.random.uniform(0.4, 10.4, 76)}
+)
+
+fig = px.choropleth_mapbox(df, geojson = neighborhoods, 
+                           locations = "fips",
+                           color="unemp",
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           featureidkey="properties.cartodb_id",
+                           mapbox_style="carto-positron",
+                           opacity=0.5,
+                           center = {"lat": 41.8781, "lon": -87.6298},
+                           zoom=9)
+fig.update_geos(fitbounds="locations",visible=False)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0},)
+# fig.update_traces(marker=dict(size=10, 
+#                               line=dict(width=3, color='white')))
+fig.show()
+
+
+
+# ------------------ html page layout here ----------------------------
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1('A Map of Chicago Neighborhoods', style={'text-align':'center'}),
+    html.P('This is a paragraph introducing stuff\nhow do i start a new line'),
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
+    html.Div('''
+        With randomly generated data
     '''),
 
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='graph',
+        figure = fig
     )
-])
+
+], style={'marginLeft': 60, 'marginTop': 60})
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
