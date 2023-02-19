f40441a8069b8e53e40c35099425ebaaddf32a00
Betty Fang <bfang@uchicago.edu>
2023-02-18 18:22:18 -0600

    removed double white space from address

--- cleaning/idhs_cleaning.py
+++ cleaning/idhs_cleaning.py
@@ -3,12 +3,16 @@ import re
 
 def clean():
     """
+    This function cleans the idhs.json file and saves the output into a file
+    named cleaned_idhs.json
     """
 
 with open("../webscraping/idhs.json", "r") as fj:
     data = json.load(fj)
     for office in data:
+        office["Name"] = re.sub(r"[\s]$","", office["Name"])
         office["Address"] = office["Address"].strip()
+        office["Address"] = re.sub(r"(\s{2,})", " ", office["Address"])
         office["Contact"] = office["Contact"].strip()
         office["Contact"] = re.sub(r"[\t]", "", office["Contact"])
         office["Contact"] = re.sub(r"[\n+]", " ", office["Contact"])
