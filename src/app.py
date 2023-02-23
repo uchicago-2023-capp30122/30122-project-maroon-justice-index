fcf5152f83ddfc181a4c0a1bf78713dc53984ac1
Ivanna Melissa Rodriguez <ivannar@linux3.cs.uchicago.edu>
2023-02-23 13:48:32 -0600

    tried census tract fig but not rendering bc of vs code error?

--- src/app.py
+++ src/app.py
@@ -10,36 +10,70 @@ import numpy as np
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# ------------------ data and map here --------------------------------
+# ------------------ data and maps here --------------------------------
 
-neighborhoods = "https://raw.githubusercontent.com/blackmad/neighborhoods/master/chicago.geojson"
+# --------- census tract pp index map ----------------
 
-df = gpd.read_file(neighborhoods)
-df['index'] = np.random.uniform(0.4, 10.4, 98) # generate random index data
+# read in geojson
+census_tracts = "boundaries_census_tracts_2010.geojson"
+census_tracts_df = gpd.read_file(census_tracts)
+# read in json
+df = pd.read_json("index_draft_disposable_income_sex_age.json")
 
-fig = px.choropleth_mapbox(df, geojson = neighborhoods, 
+# figure
+fig1 = px.choropleth_mapbox(df, 
+                           geojson = census_tracts_df, 
                            title = "Period Poverty Index by Neighborhood",
-                           locations = "cartodb_id",
-                           color="index",
+                           locations = "tract",
+                           color="pp_index",
                            color_continuous_scale="amp",
-                           range_color=(0, 12),
-                           featureidkey="properties.cartodb_id",
+                           range_color=(0, 20.7),
+                           featureidkey="properties.tractce10",
                            mapbox_style="carto-positron",
                            opacity=0.5,
-                           hover_name="name",
-                           hover_data={'cartodb_id':False}, 
+                           hover_name="NAME",
                            center={"lat": 41.8781, "lon": -87.6298},
                            zoom=9)
-fig.update_geos(fitbounds="locations",visible=False)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}#,
-                #   legend=dict( # this is not working? legend does not update
-                #       yanchor="bottom",
-                #       y=0.99,
-                #       xanchor="left",
-                #       x=0.01
-                #       )
+fig1.update_geos(fitbounds="locations",visible=False)
+fig1.update_layout(margin={"r":0,"t":0,"l":0,"b":0},
+                  legend=dict(
+                      yanchor="bottom",
+                      y=0.99,
+                      xanchor="left",
+                      x=0.01
+                      )
                   )
-fig.show()
+fig1.show()
+
+
+# neighborhoods = "https://raw.githubusercontent.com/blackmad/neighborhoods/master/chicago.geojson"
+
+# df = gpd.read_file(neighborhoods)
+# df['index'] = np.random.uniform(0.4, 10.4, 98) # generate random index data
+
+# fig = px.choropleth_mapbox(df, geojson = neighborhoods, 
+#                            title = "Period Poverty Index by Neighborhood",
+#                            locations = "cartodb_id",
+#                            color="index",
+#                            color_continuous_scale="amp",
+#                            range_color=(0, 12),
+#                            featureidkey="properties.cartodb_id",
+#                            mapbox_style="carto-positron",
+#                            opacity=0.5,
+#                            hover_name="name",
+#                            hover_data={'cartodb_id':False}, 
+#                            center={"lat": 41.8781, "lon": -87.6298},
+#                            zoom=9)
+# fig.update_geos(fitbounds="locations",visible=False)
+# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}#,
+#                 #   legend=dict( # this is not working? legend does not update
+#                 #       yanchor="bottom",
+#                 #       y=0.99,
+#                 #       xanchor="left",
+#                 #       x=0.01
+#                 #       )
+#                   )
+# fig.show()
 
 # ------------------ html page layout here ----------------------------
 
@@ -69,7 +103,7 @@ app.layout = dbc.Container([
         dbc.Col([
             dcc.Graph(
                 id='graph',
-                figure = fig)
+                figure = fig1)
         ], width=12)
     ], align='center'),
 
