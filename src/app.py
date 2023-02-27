52a96e96628076e88a3e577b7a4bc032916d2be0
Ivanna Melissa Rodriguez <ivannar@linux1.cs.uchicago.edu>
2023-02-27 14:24:45 -0600

    added dropdown

--- src/app.py
+++ src/app.py
@@ -18,51 +18,58 @@ census_tracts_df = gpd.read_file("boundaries_census_tracts_2010.geojson")
 df = pd.read_json("index_draft_disposable_income_sex_age.json")
 
 # figure
-fig1 = px.choropleth_mapbox(df, 
-                           geojson = census_tracts_df, 
-                           locations = "tract",
-                           color="pp_index",
-                           color_continuous_scale="amp",
-                           range_color=(0, 20.7),
-                           featureidkey="properties.tractce10",
-                           mapbox_style="carto-positron",
-                           opacity=0.5,
+fig_idx = px.choropleth_mapbox(df, geojson = census_tracts_df, locations = "tract",
+                           color="pp_index", color_continuous_scale="amp",
+                           range_color=(0, 20.7), featureidkey="properties.tractce10",
+                           mapbox_style="carto-positron", opacity=0.5,
                            hover_name="NAME",
-                           center={"lat": 41.8781, "lon": -87.6298},
-                           zoom=9)
-fig1.update_geos(fitbounds="locations",visible=False)
-fig1.update_layout(title_text='Period Poverty Index by Census Tract', title_x=0.5,
-    margin={"r":0,"t":40,"l":0,"b":0},
-    legend=dict(
-        yanchor="bottom",
-            y=0.99,
-            xanchor="left",
-            x=0.01
-            ))
+                           center={"lat": 41.8781, "lon": -87.6298}, zoom=9)
+fig_idx.update_geos(fitbounds="locations", visible=False)
+fig_idx.update_layout(title_text='Period Poverty Index by Census Tract', title_x=0.5,
+    margin={"r":0,"t":40,"l":0,"b":0})
+fig_idx.update_traces(marker_line_width=1, marker_line_color='white')
 
 # ---------------- community centers map --------------------
 
-df2 = pd.read_csv("geocoded_community_centers_chicago.csv")
-geometry = gpd.points_from_xy(df2['lon'], df2['lat'])
-gdf = gpd.GeoDataFrame(df2, geometry=geometry)
+df_cc = pd.read_csv("geocoded_community_centers_chicago.csv")
+geometry = gpd.points_from_xy(df_cc['lon'], df_cc['lat'])
+gdf = gpd.GeoDataFrame(df_cc, geometry=geometry)
+neighborhoods = gpd.read_file("https://raw.githubusercontent.com/blackmad/neighborhoods/master/chicago.geojson")
+joined = gpd.sjoin(gdf, neighborhoods, predicate='within')
+joined = joined.rename(columns={'name':'Neighborhood', 'Name':'Community Center'})
 
-fig2 = px.scatter_mapbox(gdf, 
-                        lat=gdf.geometry.y, 
-                        lon=gdf.geometry.x, 
+fig_cc = px.scatter_mapbox(joined, 
+                        lat=joined.geometry.y, 
+                        lon=joined.geometry.x, 
                         zoom=10,
-                        hover_name="Name",
+                        hover_name="Community Center",
+                        hover_data=['neighborhood'],
+                        # hover_data={'Neighborhood': True, 'lat':False, 'lon':False},
+                        opacity=0.5,
                         color="Type",
-                        mapbox_style="carto-positron",
-                        opacity=0.5)
-fig2.update_layout(title_text='Women and Girls Community Centers in Chicago', title_x=0.5,
-    margin={"r":0,"t":40,"l":0,"b":0})
-
-# neighborhoods = "https://raw.githubusercontent.com/blackmad/neighborhoods/master/chicago.geojson"
+                        mapbox_style="carto-positron")
+fig_cc.update_layout(title_text='Women and Girls Community Centers in Chicago', 
+    title_x=0.5, margin={"r":0,"t":40,"l":0,"b":0})
+fig_cc.update_layout({
+    'updatemenus': [{
+        'buttons': [
+            {
+                'args': [{'mapbox.zoom':12, 'mapbox.center.lat':joined[joined['Neighborhood'] == neighborhood]['lat'].iloc[0],
+                          'mapbox.center.lon': joined[joined['Neighborhood'] == neighborhood]['lon'].iloc[0]}],
+                'label': neighborhood,
+                'method': 'relayout'
+            } for neighborhood in joined['Neighborhood'].unique()
+        ],
+        'direction': 'down',
+        'showactive': True,
+        'x': 0.01,
+        'y': 0.95
+    }]
+})
 
 # --------------------------- html page layout here ----------------------------
 
 app.layout = dbc.Container([
-
     # header
     dbc.Row([
         dbc.Col([
@@ -94,8 +101,8 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             dcc.Graph(
-                id='graph',
-                figure = fig1)
+                id='map-idx',
+                figure = fig_idx)
         ], width=12)
     ], align='center'),
 
@@ -128,8 +135,8 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             dcc.Graph(
-                id='graph1',
-                figure = fig2)
+                id='map-cc',
+                figure = fig_cc)
         ], width=12)
     ], align='center'),
 
