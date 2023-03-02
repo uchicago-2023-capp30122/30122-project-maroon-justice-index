368b45b607bcfee95f38ea34a436d6f1626e35e8
Jimena Salinas Valdespino <jimenas@linux7.cs.uchicago.edu>
2023-03-02 09:57:34 -0600

    added apply prox method

--- analysis/apply_proximity_to_census.py
+++ analysis/apply_proximity_to_census.py
@@ -8,12 +8,14 @@ updating the master analysis file
 
 from analysis.proximity import proximity_analysis
 import geopandas as gpd
+import pandas as pd
+import geopandas as gpd
 
 
 class CensusTractCentroids:
     """
     This class finds the centroids for each Census tract
-    and applied the proximity analysis function to estimate
+    and applies the proximity analysis function to estimate
     proximity to resource centers.
     """
 
@@ -32,18 +34,33 @@ class CensusTractCentroids:
         GeoDataFrame
 
         Returns:
-            - List of tuples: (list) a list of tuples with 
-            the latitude and longitude of a tract centroid.
+            - Dictionary of coordinates by tract: (dict) a dictionary mapping
+            each Illinois Census tract to a tuple with the latitude and 
+            longitude of the tract's centroid .
         """
-        centroids = []
+        centroids = {}
         for row in self.tracts_geo.itertuples():
             centroid = row.geometry.centroid
+            tract = row.tractce10
             lat = centroid.y
             lon = centroid.x
-            centroids.append((lat, lon))
+            centroids[tract] = (lat, lon)
         return centroids
 
+    def apply_prox_to_census(self):
+        """
 
-# TO DO: run Betty's function
+        """
+        centroids_census = self.get_centroids()
+        number_of_centers = {}
+        for tract, coordinates in centroids_census.items():
+            lat, long = coordinates
+            centers_count = proximity_analysis(lat, long)
+            number_of_centers[tract] = centers_count
+        return number_of_centers
+
+        # TO DO: run Betty's function
 centroids = CensusTractCentroids().get_centroids()
 centroids
+centers = CensusTractCentroids()
+centers.apply_prox_to_census()
