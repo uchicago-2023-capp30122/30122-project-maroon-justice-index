f74ae6d1736db8f845017211ad444bf158ce12fb
Diamon Chanel Dunlap <diamon@linux7.cs.uchicago.edu>
2023-03-04 12:40:36 -0600

    update main

--- ppindex/__main__.py
+++ ppindex/__main__.py
@@ -1,7 +1,7 @@
 import dash
 from dash import html
 import dash_bootstrap_components as dbc
-from src.app import app
+from .src.app import app
 
 if __name__ == '__main__':
     app.run_server(debug=True)
